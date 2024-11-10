e7c86f76f561d586be7ffc8deb1efbc543ee6296
spmconf2025 <spmconf2025@gmail.com>
2024-11-09 23:56:33 -0500

    add initial file for SPM25

---
+++ SPM2025/menu.js
@@ -0,0 +1,41 @@
+document.write('\
+<div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">\
+	<ul id="nav" class="navbar-nav ms-auto">\
+		<li class="nav-item">\
+			<a id="about" class="dd-menu collapsed" href="index.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">About</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="index.html">Conference</a></li>\
+				<li class="nav-item"><a href="Organizer.html">Organizers</a></li>\
+			</ul>\
+		</li>\
+		<li class="nav-item">\
+			<a id="program" class="dd-menu collapsed" href="Program.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Program</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="CfP.html">Call for Papers</a></li>\
+				<li class="nav-item"><a href="Poster.html">Call for Posters</a></li>\
+				<li class="nav-item"><a href="CAD.html">Presentation-only Papers</a></li>\
+				<li class="nav-item"><a href="Program.html">Schedule</a></li>\
+			</ul>\
+		</li>\
+		<li class="nav-item">\
+			<a id="venue" class="dd-menu collapsed" href="Venue.html" data-bs-toggle="collapse" data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">Travel</a>\
+			<ul class="sub-menu collapse" id="submenu-1-1">\
+				<li class="nav-item"><a href="Venue.html">Venue</a></li>\
+				<li class="nav-item"><a href="Accommodation.html">Accommodation</a></li>\
+				<li class="nav-item"><a href="Local.html">Local Info</a></li>\
+			</ul>\
+		</li>\
+		<li class="nav-item"><a id="keynote" href="Keynote.html" aria-label="Toggle navigation">Keynotes</a></li>\
+		<li class="nav-item"><a href="http://solidmodeling.org/spm-conferences/" target="new" aria-label="Toggle navigation">Previous Conferences</a></li>\
+	</ul>\
+</div>\
+');
+
+/*
+		<li class="nav-item"><a id="program" href="Program.html" aria-label="Toggle navigation">Program</a></li>\
+		<li class="nav-item"><a id="award" href="Award.html" aria-label="Toggle navigation">Bézier Award</a></li>\
+		<li class="nav-item"><a id="sponsor" href="Sponsor.html" aria-label="Toggle navigation">Sponsor</a></li>\
+		
+				<li class="nav-item"><a href="Childcare.html">Childcare Services</a></li>\
+				<li class="nav-item"><a href="VisitCanada.html">Visit Canada</a></li>\
+*/
